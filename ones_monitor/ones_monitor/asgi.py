08e2bc2893e217b4b3e611d3f19f4852407bd65b
admin <linyuming@kingsoft.com>
2025-05-20 19:12:58 +0800

    2025年5月20日19:12:50

--- ones_monitor/ones_monitor/asgi.py
+++ ones_monitor/ones_monitor/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ones可视化大盘 project.
+ASGI config for ones_monitor project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ones可视化大盘.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ones_monitor.settings')
 
 application = get_asgi_application()
--- ones_monitor/ones_monitor/urls.py
+++ ones_monitor/ones_monitor/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ones可视化大盘 project.
+URL configuration for ones_monitor project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
